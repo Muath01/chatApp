a80faf48ba4364df437438a4e4f452e4b9da00d4
Muath <Muath.khalifa@yahoo.com>
2022-08-13 21:03:59 +0100

    sever

--- server.js
+++ server.js
@@ -2,13 +2,18 @@
 // const io = new server(3000, {cors:{origin:"http://localhost:3000"}})
 
 const io = require('socket.io')(3000)
-
+const cors = require("cors")
+const express = require("express");
+const app = express();
+app.use(cors({
+  origin: "*",
+}))
 const users = {}
 
 io.on('connection', socket => {
   socket.emit("chat-message", "hello World")
   socket.on("")  
   socket.on("send-chat-message", message =>{
-    console.log(message)
+    socket.broadcast.emit("chat-message", message)
   })
 })
